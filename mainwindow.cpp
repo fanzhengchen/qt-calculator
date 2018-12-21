7b174bfbca7c1d47b0994645a052acf0fe34817c
fanzhengchen <fanzhengchen@gmail.com>
2018-12-22 02:06:44 +0800

    init

---
+++ mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    printf("1\n");
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    printf("3\n");
+}
